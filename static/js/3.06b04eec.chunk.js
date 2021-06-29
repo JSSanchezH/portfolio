15ffc06bad2623aa214aae74927f1764942bd1c0
Juan Sebastian <juansebastiansanchezh@gmail.com>
2021-06-29 12:52:47 -0500

    Updates

---
+++ static/js/3.06b04eec.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpportafolio=this.webpackJsonpportafolio||[]).push([[3],{40:function(o,p,i){}}]);
+//# sourceMappingURL=3.06b04eec.chunk.js.map
--- static/js/3.af9184b1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportafolio=this.webpackJsonpportafolio||[]).push([[3],{19:function(o,p,i){}}]);
-//# sourceMappingURL=3.af9184b1.chunk.js.map
